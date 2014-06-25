01acbbac6a3f7886772f7e2185051adb51dcae45
mattheweallen <mattheweallen@gmail.com>
2014-06-25 17:20:36 -0500

    added activities controller and routes

---
+++ fitdrift/app/controllers/Activities.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by matt on 6/25/14.
+ */
+public class Activities extends Controller {
+
+    public static Result list() {
+        return TODO;
+    }
+
+    public static Result newActivity() {
+        return TODO;
+    }
+
+    public static Result details(String aid) {
+        return TODO;
+    }
+
+    public static Result save() {
+        return TODO;
+    }
+
+    public static Result delete(String aid) {
+        return TODO;
+    }
+}
