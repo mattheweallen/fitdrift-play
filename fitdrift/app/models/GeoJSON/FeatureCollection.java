c7b422bb943d0d8999f90bfe7790e4b0d83b6d11
mattheweallen <mattheweallen@gmail.com>
2014-07-07 06:08:10 -0500

    added FeatureCollection

---
+++ fitdrift/app/models/GeoJSON/FeatureCollection.java
@@ -0,0 +1,12 @@
+package models.GeoJSON;
+
+import java.util.List;
+
+/**
+ * Created by matt on 7/7/14.
+ */
+public class FeatureCollection {
+    public String type;
+    public List<Feature> features;
+
+}
